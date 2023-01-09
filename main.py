57ebed3962869909756d8345cae0965f8a9dcd99
Ferralium <jerome@DESKTOP-15G3JO2>
2023-01-09 22:02:26 +0100

    CSV inladen en aangepaste station class

--- main.py
+++ main.py
@@ -17,6 +17,26 @@ class Railsolver():
         self.stations = {}
 
     def load_stations(self):
-
     # Load functie om alles van de CSV in te laden
-    with open('data/stations') as f:
+        with open('data/ConnectiesHollandKlein.csv') as f:
+            next(f)
+            for line in f:
+                templine = line
+                templine = templine.strip().split(',')
+                if templine[0] in self.stations:
+                    # Hier functie aanroepen uit station om connectie toe te voegen met de lengte als value
+                    self.stations[templine[0]].add_station(templine[1], templine[2])
+
+                elif templine[0] not in self.stations:
+                    # Hier functie die nieuwe station aanmaakt en de connectie gelijk toevoegt
+                    self.stations[templine[0]] = Station(templine[0])
+                    self.stations[templine[0]].add_station(templine[1], templine[2])        
+                
+                # Hier tweede functie die checkt of destination station ook al bestaat, zo niet voegt deze toe
+                if templine[1] not in self.stations:
+                    self.stations[templine[1]] = Station(templine[1])
+                    self.stations[templine[1]].add_station(templine[0], templine[2])
+
+if __name__ == '__main__':
+    wisselstoring = Railsolver()
+    wisselstoring.load_stations()
--- station.py
+++ station.py
@@ -3,5 +3,11 @@ class Station:
         self.connections = {}
         self.name = stationname
         self.connectioncount = 0
+        self.isvisited = False
 
-    
+    def add_station(self, connection, distance):
+        self.connections[connection] = distance
+        self.connectioncount += 1
+
+    def stationvisit(self):
+        self.isvisited = True
