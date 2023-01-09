344d61a5cc4042009eb901d7488b9a35bfe8f4a2
Ferralium <jerome@DESKTOP-15G3JO2>
2023-01-09 22:24:51 +0100

    Opzet afmaken load functie

--- main.py
+++ main.py
@@ -13,29 +13,42 @@
 from station import Station
 
 class Railsolver():
+    
+    # Initializes the stations dictionary for the railsolver
     def __init__(self):
         self.stations = {}
 
     def load_stations(self):
-    # Load functie om alles van de CSV in te laden
+        """Loads the stations with distances from the CSV.
+           Also creates new stations for the connections if they are not already in the dict"""
+    
         with open('data/ConnectiesHollandKlein.csv') as f:
+            # Met de next functie wordt de eerste lijn overgeslagen, dit geeft alleen informatie over de inhoud
             next(f)
+
+            # For loop iterates over the lines in the csv and modifies them to usable format
             for line in f:
                 templine = line
                 templine = templine.strip().split(',')
+
+                # For loop schrijven die voor zowel source als destination station inlaadt en zo beide connecties toevoegd
+                # Belangrijk is voor beide de check of station al bestaat of niet, daarom for loop
+
+                # Checks if station already exists, if so adds connection
                 if templine[0] in self.stations:
-                    # Hier functie aanroepen uit station om connectie toe te voegen met de lengte als value
                     self.stations[templine[0]].add_station(templine[1], templine[2])
 
+                # If the station does not exist initializes the station and adds the connection
                 elif templine[0] not in self.stations:
-                    # Hier functie die nieuwe station aanmaakt en de connectie gelijk toevoegt
                     self.stations[templine[0]] = Station(templine[0])
-                    self.stations[templine[0]].add_station(templine[1], templine[2])        
-                
-                # Hier tweede functie die checkt of destination station ook al bestaat, zo niet voegt deze toe
+                    self.stations[templine[0]].add_station(templine[1], templine[2])
+
+                # Checks whether the connection already exists in the stations and adds it if this is not the case
                 if templine[1] not in self.stations:
                     self.stations[templine[1]] = Station(templine[1])
-                    self.stations[templine[1]].add_station(templine[0], templine[2])
+                    self.stations[templine[1]].add_station(templine[0], templine[2])        
+                
+                
 
 if __name__ == '__main__':
     wisselstoring = Railsolver()
