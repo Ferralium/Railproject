7088e804528495b67eb2af5867865a2e06fc5e7d
Ferralium <jerome@DESKTOP-15G3JO2>
2023-01-10 11:11:37 +0100

    Train edit

--- train.py
+++ train.py
@@ -3,9 +3,11 @@ class Train:
         self.trainname = f'train{trainnum}'
         self.route = [firststop]
         self.trainscore = 0
+        self.laststation = firststop
 
     def add_stop(self, stopname, stopscore):
         self.route.append(stopname)
         self.trainscore += stopscore
+        self.laststation = self.route[len(self.route) - 1]
 
     
