932d4044fcd20dd35fcda9013bac4513f367f3e4
Berber <berber@sp-byods-145-109-24-112.wireless.uva.nl>
2023-01-24 16:15:02 +0100

    invoegen klasse simulated annealing en de loop in de main

---
+++ algorithms/simulatedannealing.py
@@ -0,0 +1,96 @@
+import random
+# voor deze hillclimber er per stap
+# - 2 keer een random start state worden gekozen
+
+
+class SimulatedAnnealing:
+
+    def __init__(self):
+        """ Deze klas is de representatie van een hill climber algoritme om het Rail NL probleem op te lossen """
+        pass
+
+
+    def starting_station(self, station_dictionary, statnames):
+        """"Picks a purely random starting station from the list of all possible stations"""
+        current_station = random.choice(list(station_dictionary.values()))
+        print(f'random starting station: {current_station}')
+        return current_station
+
+
+    def move(self, current_station, train_stations, stations_dictionary):
+        time = 0
+
+        # voeg de current station toe aan de lijst
+        train_stations.append(current_station)
+        print(train_stations)
+
+        while True:
+
+            # Moves to next random connection that has not been visited yet
+            possible_next_station: str | Station = random.choice(list(current_station.connections))
+            next_station: Station = stations_dictionary.get(possible_next_station)
+
+
+            all_time: int = time + current_station.connections.get(str(next_station))
+
+            # stops if time is more than 3 hours
+            if all_time > 180:
+                print(f'hi {all_time}')
+                print(f'this will be returned {time}')
+                return train_stations, time
+            else:
+                time = time + current_station.connections.get(str(next_station))
+
+            print(f' Current Station: {current_station}')
+            current_station.stationvisit(str(next_station))
+            next_station.stationvisit(str(current_station))
+            current_station: Station = stations_dictionary.get(str(next_station))
+
+            # voeg current_station toe aan de lijst
+            train_stations.append(current_station)
+
+            print(f' Next Station: {current_station}')
+            print(time)
+            print(" ")
+
+    def compare_scores(self, score_new, score_old):
+        """ Function that compares the scores of the mutated and start algorithm """
+
+        delta: float = score_new - score_old
+        chance = 2**delta
+
+        return chance
+
+        #pass
+
+
+    def make_or_break_change(self):
+        """ Function that makes or breaks the mutation """
+        pass
+
+
+        if chance => 1:
+
+            # voer nieuwe state in
+            train_dictionary = option_2
+
+        if chance > 0 and chance < 1:
+
+            # afhankelijk van de kans, voer hem in:
+            guess = random.uniform(0, 1)
+
+            print(f'guess: {guess}')
+
+            if guess < chance:
+
+                train_dictionary = option_2
+
+    # def mutation(self):
+    #     """ Functie die een kleine mutatie maakt op de huidige state. """
+    #     pass
+    #     # make a mutation on this random algorithm
+    #
+    #     # hierin wordt eventueel het uiteinde van een random trein aangepast
+    #     # kies eerst een random getal tussen de 1 en 20, en een getal tussen 1 of 2 (begin of eind van de trein)
+    #     random_change = random.randint(0,19)
+    #     front_or_back = random.randint(1,2)
--- random_algorithm.py
+++ random_algorithm.py
@@ -1,12 +1,10 @@
 """
     railsolver.py
-
     Door:
         Chiara Schut - 14676869
         Berber Siersma - 14472295
         Jeroen Steenhof - 12709425
     Minor Programmeren - Algoritmen en Heuristieken
-
     Attempts to solve heuristic problem with train stations.
 """
 # TYPEHINTS!!! DOCSTRINGS!! COMMENTS!!!
@@ -24,7 +22,8 @@ from algorithms.smart_algo import SmartAlgorithm
 from algorithms.least_connections import LeastConnections
 from algorithms.ShortestTimeHeuristic import ShortestTimeHeuristic
 from algorithms.LongestTimeHeuristic import LongestTimeHeuristic
-from algorithms.MostConnectionsHeuristic import MostConnectionsHeuristic
+from algorithms.SimulatedAnnealing import SimulatedAnnealing
+
 class Railsolver():
 
     # Initializes the stations dictionary for the railsolver
@@ -87,12 +86,11 @@ class Railsolver():
         elif self.algoselector == 3:
             self.algo = LeastConnections()
         elif self.algoselector == 4:
-            self.algo = MostConnectionsHeuristic()
-        elif self.algoselector == 5:
             self.algo = ShortestTimeHeuristic()
-        elif self.algoselector == 6:
+        elif self.algoselector == 5:
             self.algo = LongestTimeHeuristic()
-        
+        elif self.algoselector == 6:
+            self.algo = SimulatedAnnealing()
 
     def quality_calc(self, fraction: float, list_of_numbers) -> None:
         T: int = list_of_numbers[1]
@@ -103,6 +101,9 @@ class Railsolver():
 
     def fraction_calc(self) -> float:
         """ Function calculates percentage of used connections """
+
+        print("Calculate franction of used connections")
+        print()
         connected = 0
         total = 0
 
@@ -133,6 +134,7 @@ class Railsolver():
         train_dictionary[train_number] = list_of_stations
 
         train_data = pd.DataFrame.from_dict(train_dictionary, orient='index')
+        print(train_data)
 
         data_to_excel = pd.ExcelWriter('train_data.xlsx')
 
@@ -140,7 +142,7 @@ class Railsolver():
         train_data.to_excel(data_to_excel)
 
         # save the excel
-        # data_to_excel.save()
+        data_to_excel.save()
 
     def take_a_ride(self):
         """ Function that keeps the order of everything that must be done for the algorithm
@@ -158,6 +160,9 @@ class Railsolver():
             # maak een lege lijst voor de stations:
             train_stations: list[Station] = []
 
+            print(" ")
+            print("new trajectory")
+
             current_station: Station = self.algo.starting_station(self.stations, self.statnames)
 
             list_of_stations_and_time: tuple[Station, int] = self.algo.move(current_station, train_stations, self.stations)
@@ -171,14 +176,37 @@ class Railsolver():
             total_time += time_trajectory
 
         list_of_numbers: list[int] = [total_time, number_of_routes]
-
+        print(f'list of numbers: {list_of_numbers}')
         return list_of_numbers
-    
-    def clear_visited_stations(self):
-        for station in self.stations:
-            temp_station = self.stations[station]
-            for connection in temp_station.connection_visited:
-                temp_station.station_unvisit(connection)
+
+
+    def loop_simulated_annealing(self, train_dictionary):
+        """ Loop that controls the simmulated annealing process """
+
+        # train_dictionary = {}
+        list_of_numbers = wisselstoring.take_a_ride()
+
+        # bereken de fractie van de bereden routes
+        fraction: float = wisselstoring.fraction_calc()
+        quality = SimulatedAnnealing.quality_calc(fraction, list_of_numbers)
+
+        # de range is nu 10 keer, dus er wordt 10 keer geprobeerd een andere oplossing te vinden
+        for i in range(10):
+
+            train_dictionary_2 = {}
+
+            list_of_numbers = wisselstoring.take_a_ride()
+
+            # bereken de fractie van de bereden routes
+            fraction: float = wisselstoring.fraction_calc()
+            quality_2 = self.algo.quality_calc(fraction, list_of_numbers)
+
+            # vergelijk nu deze met elkaar, en is het beter of de kans zegt dat het moet, verander hem dan
+            short_tuple = self.algo.make_or_break_change(quality, quality_2, train_dictionary, train_dictionary_2)
+
+            train_dictionary = short_tuple[0]
+            quality = short_tuple[1]
+
 
 
     def visualise(self, routes):
@@ -220,7 +248,7 @@ if __name__ == '__main__':
     file2.close()
 
 
-    
+
     if len(sys.argv) >= 2:
         if sys.argv[1].isnumeric() == False:
             print('Usage: python3 main.py (Optional) n')
@@ -233,39 +261,49 @@ if __name__ == '__main__':
             if int(sys.argv[2]) > 1:
                 algoselect = int(sys.argv[2])
 
+    train_dictionary = {}
 
-    for i in range(num_of_runs):
-        print(f'Currently on run number {i}')
-        wisselstoring = Railsolver(statconnectlib, statnamelib, algoselect)
+    # simulated annealing loop:
+    if algoselect == 6:
 
-        # maak een lege dictionary waarin de treinen worden opgeslagen
-        train_dictionary = {}
+        print("simulated annealing")
 
-        list_of_numbers = wisselstoring.take_a_ride()
+        wisselstoring.loop_simulated_annealing(train_dictionary)
 
-        fraction: float = wisselstoring.fraction_calc()
-        wisselstoring.quality_calc(fraction, list_of_numbers)
-        mean_solution += wisselstoring.K
-        
-        if wisselstoring.K > best_score:
-            best_score = wisselstoring.K
-            best_solution = train_dictionary
-            best_calc = wisselstoring.quality
 
-        results = open('results/resultsformula.txt', 'a')
-        results.write(f'{wisselstoring.quality}')
-        results.write('\n')
-        results.close()
+    else:
+        for i in range(num_of_runs):
+            wisselstoring = Railsolver(statconnectlib, statnamelib, algoselect)
 
-        score = open('results/score.txt', 'a')
-        score.write(str(wisselstoring.K))
-        score.write('\n')
-        score.close()
-        wisselstoring.clear_visited_stations()
+            # maak een lege dictionary waarin de treinen worden opgeslagen
+            train_dictionary = {}
+            # number_of_routes: int = 0
+            # total_time: int = 0
 
-    wisselstoring.visualise(best_solution)
-    wisselstoring.gifmod.map_to_gif()
-    print()
-    print(f'Best solution found: {best_calc}')
-    print(f'Average soluton: {mean_solution / num_of_runs}')
-    print(f'Runtime: {time.time() - start_time}')
+            list_of_numbers = wisselstoring.take_a_ride()
+
+            # bereken de fractie van de bereden routes
+            fraction: float = wisselstoring.fraction_calc()
+            wisselstoring.quality_calc(fraction, list_of_numbers)
+            mean_solution += wisselstoring.K
+
+            if wisselstoring.K > best_score:
+                best_score = wisselstoring.K
+                best_solution = train_dictionary
+                best_calc = wisselstoring.quality
+
+            results = open('results/resultsformula.txt', 'a')
+            results.write(f'{wisselstoring.quality}')
+            results.write('\n')
+            results.close()
+
+            score = open('results/score.txt', 'a')
+            score.write(str(wisselstoring.K))
+            score.write('\n')
+            score.close()
+
+        # wisselstoring.visualise(best_solution)
+        # wisselstoring.gifmod.map_to_gif()
+        print(f'Best solution found: {best_calc}')
+        print(f'Average soluton: {mean_solution / num_of_runs}')
+        print(f'Runtime: {time.time() - start_time}')
