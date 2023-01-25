88e73f65a5c98045eefc832c58d01c8c8b4e4fcf
ChiaraProgrammeerd <chiara@LAPTOP-M61H6JBC.localdomain>
2023-01-25 22:31:03 +0100

    updated 7bridges

--- heuristics/start_7bridges.py
+++ heuristics/start_7bridges.py
@@ -2,10 +2,18 @@ from station import Station
 import random
 
 def sevenbridges_start_heuristic(curr: Station, visited: list[Station], stations: dict[str, Station]):
+    """Chooses stating station with uneven number of unused connections,
+    based on the 7 bridges problem"""
 
-    current_station = random.choice(list(stations.values()))
-
-    while current_station.connection_count % 2 is 0:
+    while True:
         current_station = random.choice(list(stations.values()))
+        ununsed_connection = 0
+
+        for unvisited_connection in current_station.connection_visited.values():
+            if unvisited_connection is False:
+                ununsed_connection += 0
+
+        if not ununsed_connection % 2 is 0:
+            break
 
     return current_station
