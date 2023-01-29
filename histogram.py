19093eacb07411c74f4de7abba7d694fcfb8c243
Berber <berber@MacBook-Air-van-Berber.local>
2023-01-29 10:53:05 +0100

    background grid toevoegen

--- histogram.py
+++ histogram.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 class StatsDrawer:
     def __init__(self):
@@ -11,7 +12,10 @@ class StatsDrawer:
     def draw_hist(self):
         plt.hist(self.scorenumbers, bins = 20, edgecolor = 'black')
         plt.xlabel("Score", fontsize=13)
+        plt.xticks(np.arange(5300, 6000, step=100))
         plt.ylabel("Frequency", fontsize=13)
+        plt.yticks(np.arange(0, 3, step=1))
+        plt.grid(True, linewidth=0.5, color='#757b82', linestyle='-')
         plt.savefig('images/resultshistogram.png', dpi = 800)
         plt.close()
 
